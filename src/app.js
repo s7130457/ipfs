e18e4e686b33aa0c44322aa17c26e68db35e3019
傑尼 <s7130457@gmail.com>
2021-05-05 12:10:20 +0800

    refactory: 改寫成只有一隻app.js

--- src/app.js
+++ src/app.js
@@ -1,106 +1,55 @@
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import express from 'express'
-import session from 'express-session'
-import helmet from 'helmet'
-import httpStatus from 'http-status'
-import os from 'os'
-import routes from './routes/index.route'
-import utilsRoutes from './routes/utils.route'
-import Config from './lib/bxb_config'
-
-import { ValidationError } from 'express-validation'
-import error from './middlewares/errors/error'
-import sysStatus from './lib/bxb_status'
-import tools from '@bxb/device-tools'
-
-const http = require('http')
-
+const express = require('express')
+const cors = require('cors')
+const os = require('os')
+const multer = require('multer')
+const ipfsClient = require('ipfs-http-client')
+const ipfs = ipfsClient('http://127.0.0.1:5002')
+const STORAGE = (os.platform() !== 'linux') ? './root/storage' : '/root/storage'
+const upload = multer({ dest: STORAGE })
+const fs = require('fs')
 const app = express()
-const debug = require('debug')('bxb:app')
-const server = require('http').Server(app)
-const pkg = require('./package.json')
-const SQLiteStore = require('connect-sqlite3')(session)
-
-const ws = require('socket.io')(server, {
-  transports: ['websocket', 'polling'],
-  pingInterval: 40000,
-  pingTimeout: 25000
+app.use(cors())
+const fpath = `./ipfs.json`
+let jsonData = fs.readFileSync(fpath);
+jsonData = JSON.parse(jsonData)
+app.listen(8090, () => console.log(`server run 8090`))
+app.get('/api/media', async (req, res) => {
+  return res.status(200).json(jsonData)
 })
 
-const db = require('./db.js')
-const dbScript = require('./initDB')
-
-
-debug(`[INIT] backend ver : \x1b[31m${pkg.gitVersion}\x1b[0m`)
-
-const model = Config.get('model')
-if (os.platform() === 'linux' && tools.licenseValid(model) === false) {
-  initUnlicense()
-} else {
-  initNormal()
-}
-
-function initUnlicense () {
-  app.use(`/api/utils`, utilsRoutes)
-
-  app.listen(8090, async () => {
-    debug('\x1b[31m=====> This device is un-licensed!!! <=====\x1b[0m')
-    // desktop.send({ cmd: 'ip', show: 'true', text: 'un-licensed' })
+app.post('/api/media', upload.single('file'), async (req, res) => {
+  let chunks = []
+  const readStream = fs.createReadStream(req.file.path)
+  readStream.on('data', function(chunk) {
+    chunks.push(chunk)
   })
-}
-
-function initNormal() {
-  app.use(cors())
-  app.use(bodyParser.json({ limit: 1024 * 1024 * 1024 })) // 1G
-  app.use(bodyParser.urlencoded({ extended: true }))
-  app.use(helmet())
-
-  app.use(session({
-    secret: 'bxb9703838',
-    cookie: {
-      secure: false,
-      httpOnly: true,
-      maxAge: 8 * 60 * 60 * 1000
-    },
-    store: new SQLiteStore(),
-    resave: false,
-    saveUninitialized: true
-  }))
-
-  app.use((req, res, next) => {
-    req.ws = ws
-    next()
-  })
-
-  app.use('/api', routes)
-
-  app.use(error.converter)
-
-  app.use(error.notFound)
-
-  app.use(error.handler)
-
-  // NOTE: 這裡要用 server，不能用 app，不然 websocket 會失效
-  server.listen(8090, () => {
-    initWS(ws)
-
-    // setTimeout(() => {
-    //   // desktop.send({ cmd: 'ip', show: 'true', text: tools.getHostIp('eth0') })
-    // }, 5000)
-  })
-}
-
-function initWS (ws) {
-  setInterval(() => {
-    let ret = {}
-    try {
-      ret = sysStatus.get()
-    } catch (e) {
-      console.log(e)
+  readStream.on('end', async () => {
+    const data = await ipfs.add({
+      path: `./${req.file.originalname}`,
+      content: chunks
+    })
+    console.log(data);
+    const info = {
+      type: req.file.mimetype,
+      title: req.file.originalname,
+      id: data.path,
     }
-    ws.emit('systemStat', ret)
-  }, 1000)
-}
+    jsonData.push(info)
+    fs.writeFileSync(fpath, JSON.stringify(jsonData))
+    fs.unlinkSync(req.file.path)
+    return res.status(200).json(info)
+  })
+})
 
-export default app
+app.delete('/api/media/:id', async (req, res) => {
+  jsonData = jsonData.filter(file => file.id !== req.obj.id)
+  fs.writeFileSync(fpath, JSON.stringify(jsonData))
+  return res.status(200).json(jsonData)
+})
+
+app.param('id', (req, res, next, id) => {
+  console.log(456);
+  const obj = jsonData.find(file => file.id === id)
+  req.obj = obj
+  next()
+})
